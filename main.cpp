c16fe23682337f9ca2362b28bb4fe18d8c188844
Alice (KozlyukDA) <petrievsergey@yandex.ru>
2019-09-29 20:59:44 +0300

    code: код изменен на сумму и разность

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter A and B: ";
-int a, b;
-cin >> a >> b;
+   cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n';;
     return 0;
 }
